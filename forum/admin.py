27f3630de4922fa8565da2b6541599d2c7ea1efd
mflaviusm <flaviusmiler@yahoo.co.uk>
2022-05-03 12:10:56 +0000

    add admin post class

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
